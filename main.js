728a1b235bfd3f1677689922602295dfa035b4d5
Patrícia Belli <84143158+patriciabelli@users.noreply.github.com>
2023-10-10 18:24:33 -0300

    Add files via upload
    Projeto musical concluído.

---
+++ main.js
@@ -0,0 +1,47 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);   
+  
+    if (elemento && elemento.localName === 'audio') {
+        // console.log(elemento)
+        elemento.play( );
+    }
+
+    else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    // template string
+    const idAudio = `#som_${instrumento}`;
+
+    // console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    // console.log(contador);
+
+    tecla.onkeydown = function (evento) {
+        // console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+ 
